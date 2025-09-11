1d42dd1c989563c73c4737de92e758538e8188f5
Ruslan Giaurgiev <145995187+RuslanGiaurgiev@users.noreply.github.com>
2025-09-11 21:57:50 +0300

    Delete dealer-server directory

--- dealer-server/src/main/java/com/dealer/repository/BrandRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dealer.repository;
-
-import com.dealer.model.Brand;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrandRepository extends JpaRepository<Brand, Long> {
-}
--- dealer-server/src/main/java/com/dealer/repository/CarModelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dealer.repository;
-
-import com.dealer.model.CarModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarModelRepository extends JpaRepository<CarModel, Long> {
-}
